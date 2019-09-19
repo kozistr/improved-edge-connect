f97d65b8f2ee07c0c57690399e02e88b32611062
kozistr <kozistr@gmail.com>
2019-09-19 21:24:57 +0900

    update: vgg19 fm losses

--- src/loss.py
+++ src/loss.py
@@ -68,10 +68,10 @@ class StyleLoss(nn.Module):
 
         # Compute loss
         style_loss = .0
-        style_loss += self.criterion(self.compute_gram(x_vgg['relu2_2']), self.compute_gram(y_vgg['relu2_2']))
-        style_loss += self.criterion(self.compute_gram(x_vgg['relu3_4']), self.compute_gram(y_vgg['relu3_4']))
-        style_loss += self.criterion(self.compute_gram(x_vgg['relu4_4']), self.compute_gram(y_vgg['relu4_4']))
-        style_loss += self.criterion(self.compute_gram(x_vgg['relu5_2']), self.compute_gram(y_vgg['relu5_2']))
+        style_loss += self.criterion(self.compute_gram(x_vgg['conv2_2']), self.compute_gram(y_vgg['conv2_2']))
+        style_loss += self.criterion(self.compute_gram(x_vgg['conv3_4']), self.compute_gram(y_vgg['conv3_4']))
+        style_loss += self.criterion(self.compute_gram(x_vgg['conv4_4']), self.compute_gram(y_vgg['conv4_4']))
+        style_loss += self.criterion(self.compute_gram(x_vgg['conv5_2']), self.compute_gram(y_vgg['conv5_2']))
         return style_loss
 
 
@@ -80,6 +80,7 @@ class PerceptualLoss(nn.Module):
     Perceptual loss, VGG-based
     https://arxiv.org/abs/1603.08155
     https://github.com/dxyang/StyleTransfer/blob/master/utils.py
+    http://openaccess.thecvf.com/content_ECCVW_2018/papers/11133/Wang_ESRGAN_Enhanced_Super-Resolution_Generative_Adversarial_Networks_ECCVW_2018_paper.pdf
     """
     def __init__(self, weights=(1., 1., 1., 1., 1.)):
         super(PerceptualLoss, self).__init__()
@@ -92,12 +93,11 @@ class PerceptualLoss(nn.Module):
         x_vgg, y_vgg = self.vgg(x), self.vgg(y)
 
         content_loss = .0
-        content_loss += self.weights[0] * self.criterion(x_vgg['relu1_1'], y_vgg['relu1_1'])
-        content_loss += self.weights[1] * self.criterion(x_vgg['relu2_1'], y_vgg['relu2_1'])
-        content_loss += self.weights[2] * self.criterion(x_vgg['relu3_1'], y_vgg['relu3_1'])
-        content_loss += self.weights[3] * self.criterion(x_vgg['relu4_1'], y_vgg['relu4_1'])
-        content_loss += self.weights[4] * self.criterion(x_vgg['relu5_1'], y_vgg['relu5_1'])
-
+        content_loss += self.weights[0] * self.criterion(x_vgg['conv1_1'], y_vgg['conv1_1'])
+        content_loss += self.weights[1] * self.criterion(x_vgg['conv2_1'], y_vgg['conv2_1'])
+        content_loss += self.weights[2] * self.criterion(x_vgg['conv3_1'], y_vgg['conv3_1'])
+        content_loss += self.weights[3] * self.criterion(x_vgg['conv4_1'], y_vgg['conv4_1'])
+        content_loss += self.weights[4] * self.criterion(x_vgg['conv5_1'], y_vgg['conv5_1'])
         return content_loss
 
 
@@ -105,121 +105,121 @@ class VGG19(torch.nn.Module):
     def __init__(self):
         super(VGG19, self).__init__()
         features = models.vgg19(pretrained=True).features
-        self.relu1_1 = torch.nn.Sequential()
-        self.relu1_2 = torch.nn.Sequential()
+        self.conv1_1 = torch.nn.Sequential()
+        self.conv1_2 = torch.nn.Sequential()
 
-        self.relu2_1 = torch.nn.Sequential()
-        self.relu2_2 = torch.nn.Sequential()
+        self.conv2_1 = torch.nn.Sequential()
+        self.conv2_2 = torch.nn.Sequential()
 
-        self.relu3_1 = torch.nn.Sequential()
-        self.relu3_2 = torch.nn.Sequential()
-        self.relu3_3 = torch.nn.Sequential()
-        self.relu3_4 = torch.nn.Sequential()
+        self.conv3_1 = torch.nn.Sequential()
+        self.conv3_2 = torch.nn.Sequential()
+        self.conv3_3 = torch.nn.Sequential()
+        self.conv3_4 = torch.nn.Sequential()
 
-        self.relu4_1 = torch.nn.Sequential()
-        self.relu4_2 = torch.nn.Sequential()
-        self.relu4_3 = torch.nn.Sequential()
-        self.relu4_4 = torch.nn.Sequential()
+        self.conv4_1 = torch.nn.Sequential()
+        self.conv4_2 = torch.nn.Sequential()
+        self.conv4_3 = torch.nn.Sequential()
+        self.conv4_4 = torch.nn.Sequential()
 
-        self.relu5_1 = torch.nn.Sequential()
-        self.relu5_2 = torch.nn.Sequential()
-        self.relu5_3 = torch.nn.Sequential()
-        self.relu5_4 = torch.nn.Sequential()
+        self.conv5_1 = torch.nn.Sequential()
+        self.conv5_2 = torch.nn.Sequential()
+        self.conv5_3 = torch.nn.Sequential()
+        self.conv5_4 = torch.nn.Sequential()
 
-        for x in range(2):
-            self.relu1_1.add_module(str(x), features[x])
+        for x in range(1):
+            self.conv1_1.add_module(str(x), features[x])
 
-        for x in range(2, 4):
-            self.relu1_2.add_module(str(x), features[x])
+        for x in range(1, 3):
+            self.conv1_2.add_module(str(x), features[x])
 
-        for x in range(4, 7):
-            self.relu2_1.add_module(str(x), features[x])
+        for x in range(3, 6):
+            self.conv2_1.add_module(str(x), features[x])
 
-        for x in range(7, 9):
-            self.relu2_2.add_module(str(x), features[x])
+        for x in range(6, 8):
+            self.conv2_2.add_module(str(x), features[x])
 
-        for x in range(9, 12):
-            self.relu3_1.add_module(str(x), features[x])
+        for x in range(8, 11):
+            self.conv3_1.add_module(str(x), features[x])
 
-        for x in range(12, 14):
-            self.relu3_2.add_module(str(x), features[x])
+        for x in range(11, 13):
+            self.conv3_2.add_module(str(x), features[x])
 
-        for x in range(14, 16):
-            self.relu3_3.add_module(str(x), features[x])
+        for x in range(13, 15):
+            self.conv3_3.add_module(str(x), features[x])
 
-        for x in range(16, 18):
-            self.relu3_4.add_module(str(x), features[x])
+        for x in range(15, 17):
+            self.conv3_4.add_module(str(x), features[x])
 
-        for x in range(18, 21):
-            self.relu4_1.add_module(str(x), features[x])
+        for x in range(17, 20):
+            self.conv4_1.add_module(str(x), features[x])
 
-        for x in range(21, 23):
-            self.relu4_2.add_module(str(x), features[x])
+        for x in range(20, 22):
+            self.conv4_2.add_module(str(x), features[x])
 
-        for x in range(23, 25):
-            self.relu4_3.add_module(str(x), features[x])
+        for x in range(22, 24):
+            self.conv4_3.add_module(str(x), features[x])
 
-        for x in range(25, 27):
-            self.relu4_4.add_module(str(x), features[x])
+        for x in range(24, 26):
+            self.conv4_4.add_module(str(x), features[x])
 
-        for x in range(27, 30):
-            self.relu5_1.add_module(str(x), features[x])
+        for x in range(26, 29):
+            self.conv5_1.add_module(str(x), features[x])
 
-        for x in range(30, 32):
-            self.relu5_2.add_module(str(x), features[x])
+        for x in range(29, 31):
+            self.conv5_2.add_module(str(x), features[x])
 
-        for x in range(32, 34):
-            self.relu5_3.add_module(str(x), features[x])
+        for x in range(31, 33):
+            self.conv5_3.add_module(str(x), features[x])
 
-        for x in range(34, 36):
-            self.relu5_4.add_module(str(x), features[x])
+        for x in range(33, 35):
+            self.conv5_4.add_module(str(x), features[x])
 
         # don't need the gradients, just want the features
         for param in self.parameters():
             param.requires_grad = False
 
     def forward(self, x):
-        relu1_1 = self.relu1_1(x)
-        relu1_2 = self.relu1_2(relu1_1)
+        conv1_1 = self.conv1_1(x)
+        conv1_2 = self.conv1_2(conv1_1)
 
-        relu2_1 = self.relu2_1(relu1_2)
-        relu2_2 = self.relu2_2(relu2_1)
+        conv2_1 = self.conv2_1(conv1_2)
+        conv2_2 = self.conv2_2(conv2_1)
 
-        relu3_1 = self.relu3_1(relu2_2)
-        relu3_2 = self.relu3_2(relu3_1)
-        relu3_3 = self.relu3_3(relu3_2)
-        relu3_4 = self.relu3_4(relu3_3)
+        conv3_1 = self.conv3_1(conv2_2)
+        conv3_2 = self.conv3_2(conv3_1)
+        conv3_3 = self.conv3_3(conv3_2)
+        conv3_4 = self.conv3_4(conv3_3)
 
-        relu4_1 = self.relu4_1(relu3_4)
-        relu4_2 = self.relu4_2(relu4_1)
-        relu4_3 = self.relu4_3(relu4_2)
-        relu4_4 = self.relu4_4(relu4_3)
+        conv4_1 = self.conv4_1(conv3_4)
+        conv4_2 = self.conv4_2(conv4_1)
+        conv4_3 = self.conv4_3(conv4_2)
+        conv4_4 = self.conv4_4(conv4_3)
 
-        relu5_1 = self.relu5_1(relu4_4)
-        relu5_2 = self.relu5_2(relu5_1)
-        relu5_3 = self.relu5_3(relu5_2)
-        relu5_4 = self.relu5_4(relu5_3)
+        conv5_1 = self.conv5_1(conv4_4)
+        conv5_2 = self.conv5_2(conv5_1)
+        conv5_3 = self.conv5_3(conv5_2)
+        conv5_4 = self.conv5_4(conv5_3)
 
         out = {
-            'relu1_1': relu1_1,
-            'relu1_2': relu1_2,
+            'conv1_1': conv1_1,
+            'conv1_2': conv1_2,
 
-            'relu2_1': relu2_1,
-            'relu2_2': relu2_2,
+            'conv2_1': conv2_1,
+            'conv2_2': conv2_2,
 
-            'relu3_1': relu3_1,
-            'relu3_2': relu3_2,
-            'relu3_3': relu3_3,
-            'relu3_4': relu3_4,
+            'conv3_1': conv3_1,
+            'conv3_2': conv3_2,
+            'conv3_3': conv3_3,
+            'conv3_4': conv3_4,
 
-            'relu4_1': relu4_1,
-            'relu4_2': relu4_2,
-            'relu4_3': relu4_3,
-            'relu4_4': relu4_4,
+            'conv4_1': conv4_1,
+            'conv4_2': conv4_2,
+            'conv4_3': conv4_3,
+            'conv4_4': conv4_4,
 
-            'relu5_1': relu5_1,
-            'relu5_2': relu5_2,
-            'relu5_3': relu5_3,
-            'relu5_4': relu5_4,
+            'conv5_1': conv5_1,
+            'conv5_2': conv5_2,
+            'conv5_3': conv5_3,
+            'conv5_4': conv5_4,
         }
         return out
